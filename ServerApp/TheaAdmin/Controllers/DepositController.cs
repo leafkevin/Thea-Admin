a0cb83772df2978bf6f9f18aea8a647fdb1f244a
leafkevin <leafkevin@outlook.com>
2024-03-21 10:20:40 +0800

    update

--- ServerApp/TheaAdmin/Controllers/DepositController.cs
+++ ServerApp/TheaAdmin/Controllers/DepositController.cs
@@ -38,8 +38,8 @@ public class DepositController : ControllerBase
                 a.MemberName,
                 a.Mobile,
                 a.Balance,
-                DepositTimes = x.Count(b.DepositId).IfNull(0),
-                LastDepositedAt = x.Max(b.CreatedAt).IfNull(a.CreatedAt)
+                DepositTimes = x.Count(b.DepositId).IsNull(0),
+                LastDepositedAt = x.Max(b.CreatedAt).IsNull(a.CreatedAt)
             })
             .OrderByDescending(f => f.LastDepositedAt)
             .Page(request.PageIndex, request.PageSize)
